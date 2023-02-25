706b69caa5f7e0545a62cf340de4220cfe89432b
AkoviMensah <abmensah@wisc.edu>
2023-02-25 01:14:10 -0500

    Added feedback entity

---
+++ API/Entities/Feedback.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Text { get; set; }
+    }
+}
